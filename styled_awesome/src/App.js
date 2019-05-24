b65802c3f454ef785320254ff841522b4d85cecb
정우찬 <vdncks25@naver.com>
2019-05-24 22:04:47 +0900

    CHORE : Create React app for react-styled

---
+++ styled_awesome/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <button className="button button--success">Hello</button>
+      <button className="button button--danger">Hello</button>
+    </Fragment>
+  );
+}
+
+export default App;
