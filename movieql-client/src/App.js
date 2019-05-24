98bde9f06495b84c8170c4f66cd4b9aac79a8b37
정우찬 <vdncks25@naver.com>
2019-05-24 16:58:11 +0900

    CHORE : Set up apollo client

--- movieql-client/src/App.js
+++ movieql-client/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ApolloProvider } from "react-apollo";
+import client from "./apolloClient";
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return(
+      <ApolloProvider client={client}>
+        <div className="App" />
+      </ApolloProvider>
+    ) 
+  }
 }
-
 export default App;
